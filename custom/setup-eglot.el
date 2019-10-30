97432336ad5e8be2a1cd44f1b32f39ff6aaa4c97
malaoui <malaoui@SnIPeR>
2019-10-30 23:54:04 +0100

    dynamic xref apropos

--- custom/setup-eglot.el
+++ custom/setup-eglot.el
@@ -28,11 +28,44 @@
 (add-hook 'c-mode-hook 'c-c++-company-setup)
 (add-hook 'c++-mode-hook 'c-c++-company-setup)
 
+(defun dynamic-xref-apropos ()
+  (interactive)
+  (let ((buf (current-buffer)))
+    (ivy-read "Search for pattern: "
+              (lambda (str)
+                (cond
+                 ((< (length str) 1) nil)
+                 (t
+                  (with-current-buffer buf
+                    (when-let ((backend (xref-find-backend)))
+                      (unless (eq backend 'etags)
+                        (mapcar
+                         (lambda (xref)
+                           (let ((loc (xref-item-location xref)))
+                             (propertize
+                              (concat
+                               (when (xref-file-location-p loc)
+                                 (with-slots (file line column) loc
+                                   (format "%s:%s:%s:"
+                                           (propertize (file-relative-name file)
+                                                       'face 'compilation-info)
+                                           (propertize (format "%s" line)
+                                                       'face 'compilation-line
+                                                       )
+                                           column)))
+                               (xref-item-summary xref))
+                              'xref xref)))
+                         (xref-backend-apropos backend str))))))))
+              :dynamic-collection t
+              :action (lambda (item)
+                        (xref--pop-to-location (get-text-property 0 'xref item))))))
+
 
 (define-key c-mode-base-map (kbd "M-,") 'xref-find-references)
 (define-key c-mode-base-map (kbd "M-.") 'xref-find-definitions)
 (define-key c-mode-base-map (kbd "M-*") 'xref-pop-marker-stack)
 (define-key c-mode-base-map (kbd "M-i") 'helm-semantic-or-imenu)
+(define-key c-mode-base-map (kbd "C-M-w") 'dynamic-xref-apropos)
 
 
 
